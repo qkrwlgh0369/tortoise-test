c6d462aef8eea4413730472aefe9fb0d6eea6f07
user1 <user1@gmail.com>
2021-04-06 17:29:00 +0900

    add how to use

--- wordCounter.py
+++ wordCounter.py
@@ -1,5 +1,7 @@
 import sys
 
+print("사용방법 : [파일이름] [찾고싶은 단어]") 
+
 # 첫 번째 인자로는 파일 이름, 두 번째 인자로는 찾을 단어를 입력받는다.
 file_name =  sys.argv[1]
 word = sys.argv[2]
@@ -8,5 +10,5 @@ word = sys.argv[2]
 with open(file_name, 'rt', encoding='UTF8') as file:
     x = file.read()
 
-# 단어가 몇 번 나오는지 세고 출력한다.
+# 단어가 몇 번  나오는지 세고 출력한다.
 print(x.count(word)+"번")
