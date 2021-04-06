4916009b15589e201306dc73584c27797cc08227
qkrwlgh0369 <37234374+qkrwlgh0369@users.noreply.github.com>
2021-04-06 15:02:13 +0900

    add code

---
+++ wordCounter.py
@@ -0,0 +1,12 @@
+import sys
+
+# 첫 번째 인자로는 파일 이름, 두 번째 인자로는 찾을 단어를 입력받는다.
+file_name =  sys.argv[1]
+word = sys.argv[2]
+
+# 파일을 열고 읽는다.
+with open(file_name, 'rt', encoding='UTF8') as file:
+    x = file.read()
+
+# 단어가 몇 번 나오는지 세고 출력한다.
+print(x.count(word))
